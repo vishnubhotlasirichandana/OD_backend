3e1872c9fb9b202284f7b2bd0921ba952336777b
srinvasreddyy <sanikommusrinivasareddy2@gmail.com>
2025-08-30 15:01:03 +0530

    dotenv error

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -1,6 +1,10 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('Attempting to load Client ID:', process.env.GOOGLE_CLIENT_ID);
 
 passport.use(
   new GoogleStrategy(
